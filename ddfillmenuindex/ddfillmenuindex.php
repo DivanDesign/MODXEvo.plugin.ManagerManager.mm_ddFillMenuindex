5483e7199c879db75f8a30fba8c9425d85eaad70
Ilyas Ronef <ilyas@divandesign.biz>
2016-07-01 15:57:02 +0500

    * Minor refactoring and code style changes.

--- ddfillmenuindex/ddfillmenuindex.php
+++ ddfillmenuindex/ddfillmenuindex.php
@@ -9,7 +9,7 @@
  * 
  * @uses ManagerManager plugin 0.5.
  * 
- * @param $parent {integer; ''} - Id of parent document. Default: '' (all parents).
+ * @param $parentId {integer; ''} - Id of parent document. Default: '' (all parents).
  * 
  * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.1
  * 
@@ -17,17 +17,17 @@
  * http://www.DivanDesign.biz
  */
 
-function mm_ddFillMenuindex($parent = ''){
+function mm_ddFillMenuindex($parentId = ''){
 	global $modx, $content;
 	$e = &$modx->Event;
 	$siteContentTable = ddTools::$tables['site_content'];
 	
 	//Если у нас правильное событие и это создание документа
 	if ($e->name == 'OnDocFormPrerender' && $_REQUEST['a'] == 4){
-		$pid = intval($_REQUEST['pid']);
+		$documentParentId = intval($_REQUEST['pid']);
 		
 		//Если задан конкретный родитель, для которого должен работать виджет и он не совпадает с тем, что сейчас
-		if ($parent !== '' && $parent != $pid){
+		if ($parentId !== '' && $parentId != $documentParentId){
 			//Давай, до свидания!
 			return;
 		}
@@ -36,16 +36,16 @@ function mm_ddFillMenuindex($parent = ''){
 		$freeMenuIndex = $modx->db->getValue('
 			SELECT
 				min(`sc`.`menuindex`)
-			FROM '. $siteContentTable .' AS `sc`
+			FROM '.$siteContentTable.' AS `sc`
 				LEFT JOIN (
 					SELECT `sc1`.`menuindex`
-					FROM '. $siteContentTable .' AS `sc1`, '. $siteContentTable .' AS `sc2`
-					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$pid.' AND `sc2`.`parent` = '.$pid.'
+					FROM '.$siteContentTable.' AS `sc1`, '.$siteContentTable.' AS `sc2`
+					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$documentParentId.' AND `sc2`.`parent` = '.$documentParentId.'
 				) AS `z`
 				ON `sc`.`menuindex` = `z`.`menuindex`
 			WHERE
 				`z`.`menuindex` IS NULL AND
-				`sc`.`parent` = '.$pid.'
+				`sc`.`parent` = '.$documentParentId.'
 		');
 		
 		//Если такового нет (дочерних вообще нет). P.S.: Если он пуст, то переопределять нет смысла, там всё хорошо и так поставится.
@@ -57,7 +57,7 @@ function mm_ddFillMenuindex($parent = ''){
 		//Инкрементируем menuindex при копировании
 		$modx->db->query('
 			UPDATE
-				'. $siteContentTable .'
+				'.$siteContentTable.'
 			SET
 				`menuindex` = `menuindex` + 1
 			WHERE
